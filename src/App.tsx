fdd28bdbc3f29d6f007dac3decbddbc5927ea81d
Oscar Bazan <oscaribh97@gmail.com>
2026-02-03 14:41:50 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <iframe data-testid="embed-iframe" src="https://open.spotify.com/embed/album/5FYFgRnlsl9Qrm1xD8cqo7?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </>
+  )
+}
+
+export default App
