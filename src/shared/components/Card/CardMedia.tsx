3596f74b6d838fc76e58b3a7878c4f4bc348f1a8
Oscar Bazan <oscaribh97@gmail.com>
2026-02-17 17:38:03 -0600

    adding CardMedia component

---
+++ src/shared/components/Card/CardMedia.tsx
@@ -0,0 +1,78 @@
+import "./Card.css";
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import type { FC } from "react";
+
+interface Songs {
+    track: number;
+    name: string;
+    singer: string;
+    time?: string;
+}
+
+interface Album {
+    albumName: string;
+    albumSinger: string;
+    albumCover: string;
+    albumTracks: Songs[];
+}
+
+interface Props {
+    albumInfo: Album;
+    onSongClicked: (song: Songs) => void;
+}
+
+export const CardMedia: FC<Props> = ({ albumInfo, onSongClicked }) => {
+    const { albumName, albumSinger, albumCover, albumTracks } = albumInfo;
+    return (
+        <Card className="flex lg:flex-row md:flex-row sm:flex-col max-sm:flex-col"
+            sx={{
+                backgroundColor: 'transparent',
+                boxShadow: 'none',
+                border: 'none'
+            }}>
+            <img
+                className="w-full h-full md:w-80 md:h-80 rounded-md"
+                src={albumCover}
+                alt={albumName}
+            />
+            <Box className="flex flex-col w-full pt-2 md:pl-2 md:pt-0">
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                    <h1 className="text-center text-2xl font-medium md:text-left text-white bt-0">{albumName}</h1>
+                    <h2 className="text-center text-white mb-1 md:text-left">{albumSinger}</h2>
+                    <div className="songs-card w-full max-h-68 md:max-h-55 overflow-y-auto">
+                        {
+                            albumTracks.map((song) => (
+                                <div className="group flex items-center justify-between p-2 rounded-md hover:bg-gray-400/20 transition-colors cursor-default select-none cursor-pointer" key={song.track}
+                                    onClick={() => onSongClicked(song)}>
+
+                                    <div className="flex items-center gap-4">
+                                        <div className="w-5 flex justify-center items-center">
+                                            <span className="text-gray-500 text-sm group-hover:hidden">{song.track}</span>
+
+                                            <svg className="hidden group-hover:block w-3 h-3 text-white fill-current" viewBox="0 0 24 24">
+                                                <path d="M7 6v12l10-6z"></path>
+                                            </svg>
+                                        </div>
+
+                                        <div className="flex flex-col text-left">
+                                            <h3 className="text-white font-medium leading-tight text-[15px] w-70">{song.name}</h3>
+                                            <p className="text-[13px] text-gray-400 group-hover:text-white">{song.singer}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex items-center gap-4">
+                                        <span className="text-gray-400 text-sm font-light w-10 text-right">{song.time}</span>
+                                    </div>
+
+                                </div>
+                            ))
+                        }
+                    </div>
+                </CardContent>
+            </Box>
+        </Card>
+    );
+}
