03b44a19a4d7371d0f811d77d5efe602362c329f
Oscar Bazan <oscaribh97@gmail.com>
2026-02-18 14:45:10 -0600

    setting layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HeaderMenu } from './shared/components/Layout/HeaderMenu.tsx'
 import App from './App.tsx'
+import { Footer } from './shared/components/Layout/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <HeaderMenu />
     <App />
+    <Footer />
   </StrictMode>,
 )
