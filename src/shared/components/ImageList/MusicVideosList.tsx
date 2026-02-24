d8d135049b69ab47d73a7891a7e5de2fafd8f921
Oscar Bazan <oscaribh97@gmail.com>
2026-02-24 16:56:59 -0600

    change MusicVideosList component

--- src/shared/components/ImageList/MusicVideosList.tsx
+++ src/shared/components/ImageList/MusicVideosList.tsx
@@ -1,5 +1,5 @@
 import { ImageList, ImageListItem, ImageListItemBar, useMediaQuery, useTheme } from "@mui/material"
-import { useState } from "react";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 interface VideoList {
     title: string;
@@ -9,8 +9,6 @@ interface VideoList {
 interface Props {
     videosList: VideoList[];
 }
-
-
 
 export const MusicVideosList = ({ videosList }: Props) => {
 
@@ -25,14 +23,29 @@ export const MusicVideosList = ({ videosList }: Props) => {
     };
 
     return (
-        <ImageList cols={getCols()} gap={12} sx={
-            {
-                width: '100%', height: 'auto'
-            }}>
+        <ImageList cols={getCols()} gap={12} sx={{ width: '100%', height: 'auto' }}>
             {videosList.map((item) => (
                 <ImageListItem key={item.img}>
-                    <img srcSet={item.img} src={item.img} alt={item.title} loading="lazy" className="rounded-md" />
-                    <ImageListItemBar title={item.title} position="below" className="text-white" />
+                    <div className="group relative w-full cursor-pointer overflow-hidden">
+                        <img srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                            src={`${item.img}?w=248&fit=crop&auto=format`}
+                            alt={item.title}
+                            loading="lazy"
+                            className="rounded-md" />
+                        <div className="absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <div className="text-center text-white">
+                                <PlayCircleIcon sx={{ fontSize: 50 }} />
+                            </div>
+                        </div>
+                    </div>
+                    <ImageListItemBar title={item.title} position="below" className="text-white" sx={{
+                        '& .MuiImageListItemBar-title': {
+                            whiteSpace: 'nowrap',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            display: 'block',
+                        }
+                    }} />
                 </ImageListItem>
             ))}
         </ImageList>
