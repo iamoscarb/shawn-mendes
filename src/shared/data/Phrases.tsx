8de7ea1c1f54abdcedc2ec99ad99f6c40173d191
Oscar Bazan <oscaribh97@gmail.com>
2026-03-04 18:04:39 -0600

    adding phares component

---
+++ src/shared/data/Phrases.tsx
@@ -0,0 +1,6 @@
+export const PHRASES = [
+    `Opps... Looks like something's missing`,
+    "I don't know why, why, why",
+    "Everything means nothing if I can't have you",
+    "We're lost, lost, lost *with echo*"
+]
