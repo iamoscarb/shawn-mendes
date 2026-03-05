1eae6da323e9eaf484fcf1a89dc92b4119beb799
Oscar Bazan <oscaribh97@gmail.com>
2026-03-04 18:04:11 -0600

    editing not found page

--- src/shawnMendes/pages/NotFoundPage.tsx
+++ src/shawnMendes/pages/NotFoundPage.tsx
@@ -1,6 +1,22 @@
+import { Box } from "@mui/material"
+import { useEffect, useState } from "react"
+import { PHRASES } from "../../shared/data/Phrases";
 
 export const NotFoundPage = () => {
+
+    const array = PHRASES;
+    const [title, setTitle] = useState(array[0])
+
+    useEffect(() => {
+        document.title = 'Page Not Found';
+        const randomIndex = Math.floor(Math.random() * array.length);
+        setTitle(array[randomIndex])
+    }, []);
+
     return (
-        <div>Opps, something went wrong</div>
+        <Box sx={{ height: '69vh', backgroundColor: 'secondary.main' }} className="h-screen text-center px-10 content-center">
+            <h1 className="text-3xl font-bold text-white">{title.toUpperCase()}</h1>
+            <p className="text-2xl font-light text-white pt-3">The page you requested does not exist or isn't avaliable</p>
+        </Box>
     )
 }
