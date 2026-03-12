632960e13f75dd25a401b044689df17fabb5ef1d
Oscar Bazan <oscaribh97@gmail.com>
2026-03-11 18:08:21 -0600

    adding ShawnData data file

--- src/shared/data/ShawnData.ts
+++ src/shared/data/ShawnData.ts
@@ -4,9 +4,10 @@ export const ShawnData: AlbumInfo[] = [
     { //Shawn - The Album
         albumName: 'Shawn',
         albumSinger: 'Shawn Mendes',
+        albumKey: 'shawnAlbum',
         albumCover: 'https://m.media-amazon.com/images/I/71ewAN10xmL._UF1000,1000_QL80_.jpg',
         bannerImages: {
-            desktopBanner: 'https://image-cdn-ak.spotifycdn.com/image/ab67618600009d804dc4799c26c7f6ae1adef325',
+            desktopBanner: '../banners/shawn-album-hero-desktop.webp',
             mobilBanner: 'https://images.genius.com/5fd36429abce8d036b835539485ed9f4.737x737x1.jpg'
         },
         albumTracks: [
@@ -40,6 +41,50 @@ export const ShawnData: AlbumInfo[] = [
             { title: 'abx', img: 'https://www.shawnmendesbrasil.com/galeria/albums/userpics/10002/IMG_8994.jpg' },
             { title: 'shirtless shawn', img: 'https://www.shawnmendesbrasil.com/galeria/albums/userpics/10002/IMG_8428~0.JPG' },
             { title: 'shawn', img: 'https://www.shawnmendesbrasil.com/galeria/albums/userpics/10002/IMG_8485.JPG' },
+        ]
+    },
+    { //Wonder - The Album
+        albumName: 'Wonder',
+        albumSinger: 'Shawn Mendes',
+        albumKey: 'wonderAlbum',
+        albumCover: 'https://m.media-amazon.com/images/I/71Q9an5gVGS._UF1000,1000_QL80_.jpg',
+        bannerImages: {
+            desktopBanner: '../banners/wonder-album-hero-desktop.webp',
+            mobilBanner: 'https://images.genius.com/5fd36429abce8d036b835539485ed9f4.737x737x1.jpg'
+        },
+        albumTracks: [
+            { name: "Intro", singer: 'Shawn Mendes', time: "1:00", track: 1 },
+            { name: "Wonder", singer: 'Shawn Mendes', time: "1:00", track: 2, url: 'lS8Ps16Eelg?si=RcL5g2wtZ8ssYLTD' },
+            { name: "Higher", singer: 'Shawn Mendes', time: "1:00", track: 3 },
+            { name: "24 Hours", singer: 'Shawn Mendes', time: "1:00", track: 4 },
+            { name: "Teach Me How To Love", singer: 'Shawn Mendes', time: "1:00", track: 5 },
+            { name: "Call My Friends", singer: 'Shawn Mendes', time: "1:00", track: 6 },
+            { name: "Dreams", singer: 'Shawn Mendes', time: "1:00", track: 7 },
+            { name: "Song For No One", singer: 'Shawn Mendes', time: "1:00", track: 8 },
+            { name: "Monster", singer: 'Shawn Mendes & Justin Bieber', time: "1:00", track: 9 },
+            { name: "305", singer: 'Shawn Mendes', time: "1:00", track: 10 },
+            { name: "Always Been You", singer: 'Shawn Mendes', time: "1:00", track: 11 },
+            { name: "Piece Of You", singer: 'Shawn Mendes', time: "1:00", track: 12 },
+            { name: "Look Up At The Stars", singer: 'Shawn Mendes', time: "1:00", track: 13 },
+            { name: "Can't Imagine", singer: 'Shawn Mendes', time: "1:00", track: 14 }
+
+        ],
+        albumLink: [
+            { key: 'spotify', platform: 'Spotify', link: 'https://open.spotify.com/intl-es/album/5FYFgRnlsl9Qrm1xD8cqo7' },
+            { key: 'amazonMusic', platform: 'Amazon Music', link: 'https://music.amazon.com.mx/albums/B0DC759W8V' },
+            { key: 'appleMusic', platform: 'Apple Music', link: 'https://music.apple.com/es/album/shawn/1761182577' },
+            { key: 'otherMusic', platform: 'More', link: 'https://shawn.lnk.to/album' }
+        ],
+        videosList: [
+            { title: 'Shawn Mendes - Intro (Wonder Trailer)', img: 'https://is1-ssl.mzstatic.com/image/thumb/Video114/v4/65/bd/4f/65bd4f59-7621-f4af-7479-072ae027416a/20UMGIM86137.crop.jpg/1040x1040mv.webp' },
+            { title: 'Shawn Mendes - Wonder', img: 'https://is1-ssl.mzstatic.com/image/thumb/Video124/v4/aa/22/25/aa2225f0-50b7-ab3b-32ae-27d11498ff22/20UMGIM83365.crop.jpg/1040x1040mv.webp' },
+            { title: 'Shawn Mendes & Justin Bieber - Monster', img: 'https://is1-ssl.mzstatic.com/image/thumb/Video124/v4/88/71/fe/8871fe0b-c17f-151a-f97a-e707352d808b/20UM1IM02920.crop.jpg/1040x1040mv.webp' },
+        ],
+        photoList: [
+            { title: 'shawnph', img: 'https://i.pinimg.com/1200x/50/06/44/500644ae4e483caaec522a6ee026ad35.jpg' },
+            { title: 'wonder', img: 'https://i.pinimg.com/736x/59/d9/fc/59d9fcc5370e6a867d3aa8e00ea53148.jpg' },
+            { title: 'wonder shawn', img: 'https://i.pinimg.com/1200x/b4/01/03/b40103a7d34a98cd940d3259e2f90dc6.jpg' },
+            { title: 'shawn', img: 'https://i.pinimg.com/736x/3b/86/e5/3b86e54a2b3e525662edc2e78a321133.jpg' },
         ]
     }
 ]
