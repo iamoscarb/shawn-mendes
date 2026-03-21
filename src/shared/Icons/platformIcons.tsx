cdd2bd754ea0d7ce7f1438fd6476212f1b66c08d
Oscar Bazan <oscaribh97@gmail.com>
2026-03-20 18:13:56 -0600

    change in platformIcons

--- src/shared/Icons/platformIcons.tsx
+++ src/shared/Icons/platformIcons.tsx
@@ -2,10 +2,20 @@ import type { JSX } from "react";
 import { AmazonMusicIcon } from "./AmazonMusicIcon";
 import { AppleMusicIcon } from "./AppleMusic";
 import { SpotifyIcon } from "./SpotifyIcon";
+import { Typography } from "@mui/material";
 
 export const platformIcons: Record<string, JSX.Element> = {
-    spotify: <SpotifyIcon className="w-30" fill={'primary.main'} />,
+    spotify: <SpotifyIcon className="w-30" sx={{ color: 'primary.main' }} />,
     amazonMusic: <AmazonMusicIcon className="w-30" fill={'primary.main'} />,
     appleMusic: <AppleMusicIcon className="w-30" fill={'primary.main'} />,
-    otherMusic: <h2 className="font-bold">MORE</h2>
+    otherMusic: <Typography
+        variant="h5"
+        sx={{
+            color: 'primary.main',
+            fontWeight: 'bold',
+        }}
+        className="font-bold text-2xl"
+    >
+        MORE
+    </Typography>
 }
