b9af135d10a47bb49876683550d7246404e6ef8f
Oscar Bazan <oscaribh97@gmail.com>
2026-04-16 16:58:25 -0600

    change in MusicVideoList components class

--- src/shared/components/ImageList/MusicVideosList.tsx
+++ src/shared/components/ImageList/MusicVideosList.tsx
@@ -15,12 +15,11 @@ export const MusicVideosList = ({ videosList }: Props) => {
         <ImageList cols={getCols()} gap={12} sx={{ width: '100%', height: 'auto' }}>
             {videosList.map((item) => (
                 <ImageListItem key={item.img}>
-                    <div className="group relative w-full cursor-pointer overflow-hidden">
+                    <div className="group relative w-full cursor-pointer overflow-hidden rounded-lg border-solid border-2 border-white" >
                         <img srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                             src={`${item.img}?w=248&fit=crop&auto=format`}
                             alt={item.title}
-                            loading="lazy"
-                            className="rounded-lg border-solid border-1 border-white" />
+                            loading="lazy" />
                         <div className="absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                             <div className="text-center text-white">
                                 <PlayCircleIcon sx={{ fontSize: 50 }} />
