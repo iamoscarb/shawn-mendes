755709553ee0102f2db775467d394c942e380803
Oscar Bazan <oscaribh97@gmail.com>
2026-04-28 17:19:52 -0600

    add autoplay to DialogVideo component

--- src/shared/components/Dialog/DialogVideo.tsx
+++ src/shared/components/Dialog/DialogVideo.tsx
@@ -3,6 +3,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import type { FC } from 'react';
 import type { Songs } from '../../../interfaces/music.interface';
+import { YOUTUBE_LINK } from '../../data/Links';
 
 interface Props {
     song: Songs;
@@ -21,7 +22,7 @@ export const DialogVideo: FC<Props> = ({ song, openDialog, closeDialog }) => {
             <DialogContent className='relative p-0'>
                 <div className="relative w-full aspect-video">
                     <iframe className='absolute inset-0 w-full h-full rounded-lg'
-                        src={`https://www.youtube.com/embed/${song.url}`}
+                        src={`${YOUTUBE_LINK}/${song.url}?autoplay=1`}
                         title={`${song.singer} - ${song.name}`}
                         allow="autoplay; fullscreen" allowFullScreen></iframe>
                 </div>
