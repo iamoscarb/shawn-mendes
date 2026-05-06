5f3ca18b27453ef736e6a4b32edab612a3bcdcc2
Oscar Bazan <oscaribh97@gmail.com>
2026-05-05 18:12:01 -0600

    change in Phrases.ts

--- src/shared/data/Phrases.ts
+++ src/shared/data/Phrases.ts
@@ -2,5 +2,13 @@ export const PHRASES = [
     `Opps... Looks like something's missing`,
     "I don't know why, why, why",
     "Everything means nothing if I can't have you",
+    "Where do we go? We don't know",
+    "I don't wanna know what it's like when you're gone for good",
+    "So I'm just tryna hold on",
+    "Now I need someone to breathe me back",
+    "You watch me bleed until I can't breathe",
+    "I wonder if I'm being real",
+    "Do I speak my truth?",
+    "I wanna hold on tight",
     "We got lost, lost, lost *with echo*"
 ]
