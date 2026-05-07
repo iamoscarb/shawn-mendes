0165617dbfb684414b8d53290ad5d075bb6268f9
Oscar Bazan <oscaribh97@gmail.com>
2026-05-06 18:25:47 -0600

    change in InfoMedia custom component

--- src/shared/components/InfoMedia/InfoMedia.tsx
+++ src/shared/components/InfoMedia/InfoMedia.tsx
@@ -1,6 +1,7 @@
-import { IconButton, Popover } from "@mui/material"
+import { IconButton } from "@mui/material"
 import InfoIcon from '@mui/icons-material/Info'
-import { useState, type PropsWithChildren } from "react"
+import { type PropsWithChildren } from "react"
+import { CustomTooltip } from "../CustomTooltip/CustomTooltip";
 
 interface MediaData {
     mediaName: string;
@@ -14,32 +15,13 @@ interface Props extends PropsWithChildren {
     mediaData: MediaData
 }
 export const InfoMedia = ({ children, mediaData }: Props) => {
-    const [openMessage, setOpenMessage] = useState<HTMLElement | null>(null);
-
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setOpenMessage(event.currentTarget);
-    }
-
-    const handleClose = () => {
-        setOpenMessage(null);
-    }
-
-    const open = Boolean(openMessage);
 
     return (
         <div className="relative w-full shrink-0">
             {children}
             <div className="absolute inset-0 flex items-start justify-end">
                 <div className="flex flex-col items-end">
-                    <IconButton aria-label="info" size="large" color="primary" onClick={handleClick}>
-                        <InfoIcon fontSize="large" />
-                    </IconButton>
-
-                    <Popover id='documentary' open={open} onClose={handleClose} anchorEl={openMessage}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}>
+                    <CustomTooltip title={
                         <div className="p-3 text-neutral-700">
                             <h1 className="font-lato font-bold text-lg text-center">{mediaData.mediaName}</h1>
                             <div className="flex flex-row justify-between py-2">
@@ -52,12 +34,12 @@ export const InfoMedia = ({ children, mediaData }: Props) => {
                                     {mediaData.mediaSynopsis}
                                 </p>
                             </div>
-
                         </div>
-
-                    </Popover>
-
-
+                    }>
+                        <IconButton aria-label="info" size="large" color="primary">
+                            <InfoIcon fontSize="large" />
+                        </IconButton>
+                    </CustomTooltip>
                 </div>
             </div>
         </div>
